0f42fa06800d253e02c979350f3547be67445df2
challa prathyusha <cprathyusha94@gmail.com>
2024-04-28 10:26:42 -0500

    adding files to the git

--- archivingfiles.sh
+++ archivingfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SOURCEDIR="$1"
-
+DESTINATIONDIR="$2"
 if [ -d $SOURCEDIR ]
 then
     echo "$SOURCEDIR exists"
@@ -14,9 +14,9 @@ FILES=$( find . -name '*.log' -mtime +14)
 echo "Files that are more than 14days old:$FILES"
 
 while read -r line
-do 
+do
     echo "files to archive:$line"
-    rm $SOURCEDIR/*.log.gz
-    gzip $line
-
+    tar cvf $line.tar $line
+    mv $line.tar $DESTINATIONDIR
+    #ARCHIVEDFILES=$DESTINATIONDIR/$line.tar
 done <<< $FILES
