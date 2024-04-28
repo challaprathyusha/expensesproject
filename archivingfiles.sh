bfe8e8e7634f715b587caa0398ae4e4daf7dcc3c
challa prathyusha <cprathyusha94@gmail.com>
2024-04-27 23:44:21 -0500

    adding files to the git

---
+++ archivingfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SOURCEDIR="$1"
+
+if [ -d $SOURCEDIR ]
+then
+    echo "$SOURCEDIR exists"
+else
+    echo "Please make sure $SOURCEDIR exists"
+    exit 1
+fi
+
+cd $SOURCEDIR
+FILES=$(find . -name *.log -mtime +14)
+
+while read -r line
+do 
+    echo "files to archive:$line"
+    
+done <<< $FILES
