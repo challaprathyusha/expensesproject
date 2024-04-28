b701233420277e6818b938b856eb1065aea905d7
challa prathyusha <cprathyusha94@gmail.com>
2024-04-28 10:33:28 -0500

    adding files to the git

--- archivingfiles.sh
+++ archivingfiles.sh
@@ -17,7 +17,7 @@ else
     exit 1
 fi
 
-cd $SOURCEDIR
-FILES=$( find . -name '*.log' -mtime +14)
+
+FILES=$( find $SOURCEDIR -name '*.log' -mtime +14)
 echo "Files that are more than 14days old:$FILES"
 
