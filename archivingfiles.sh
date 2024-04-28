345377d3ee605256734778c31dae0a84374fafaf
challa prathyusha <cprathyusha94@gmail.com>
2024-04-28 10:31:03 -0500

    adding files to the git

--- archivingfiles.sh
+++ archivingfiles.sh
@@ -9,14 +9,15 @@ else
     exit 1
 fi
 
+if [ -d $DESTINATIONDIR ]
+then
+    echo "$DESTINATIONDIR exists"
+else
+    echo "Please make sure $DESTINATIONDIR exists"
+    exit 1
+fi
+
 cd $SOURCEDIR
 FILES=$( find . -name '*.log' -mtime +14)
 echo "Files that are more than 14days old:$FILES"
 
-while read -r line
-do
-    echo "files to archive:$line"
-    tar cvf $line.tar $line
-    mv $line.tar $DESTINATIONDIR
-    #ARCHIVEDFILES=$DESTINATIONDIR/$line.tar
-done <<< $FILES
