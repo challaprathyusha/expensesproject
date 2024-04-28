405b2ea02c04d9dff3d60cb0e3993a4079a94fbb
challa prathyusha <cprathyusha94@gmail.com>
2024-04-27 23:19:52 -0500

    adding files to the git

--- mysql.sh
+++ mysql.sh
@@ -7,19 +7,19 @@ checkuser
 echo "please enter password of root user:"
 read MYSQLPASSWD
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGFILE
 validate $? "Installation of mysql-server is"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGFILE
 validate $? "enabling of mysql-server is"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOGFILE
 validate $? "starting of mysql-server is"
 
-mysql -h db.expensesnote.site -uroot -p$MYSQLPASSWD -e "show databases;"
+mysql -h db.expensesnote.site -uroot -p$MYSQLPASSWD -e "show databases;" &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass $MYSQLPASSWD
+    mysql_secure_installation --set-root-pass $MYSQLPASSWD &>>$LOGFILE
     validate $? "mysql secure installation is"
 else
     echo "mysql root password is already set so....$Y SKIPPING $N"
