931343c02f38f3881c26eb325752ff6899601397
challa prathyusha <cprathyusha94@gmail.com>
2024-05-01 00:03:22 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -1,4 +1,32 @@
 SOURCEFILE="$1"
 
+if [ $# -ne 0 ]
+then
+    echo "textfile passed as an argument from commandline"
+else
+    echo  "Please mention the path of a textfile from commandline"
+    exit 1
+fi
+
+if [ -f $SOURCEFILE ]
+then
+    echo "file exists with name textfile in path:$SOURCEFILE"
+else
+    echo "file does not exist in the provide path:$SOURCEFILE"
+    exit 1
+fi
+
+checkcommand(){
+if [ $? -eq 0 ]
+then
+    echo "the command executed successfully"
+else    
+    cho "once check the command and execute"
+    exit 1
+fi
+}
+
 CONTENT=$(cat $SOURCEFILE)
-echo $CONTENT|tr [A-Z]  [a-z]|tr " " "\n"|sort -u
+UNIQWORDS=$(echo $CONTENT|tr [A-Z]  [a-z]|tr " " "\n"|sort -u)
+echo "unique words in a file:$UNIQWORDS"
+echo $UNIQWORDS|wc -w
