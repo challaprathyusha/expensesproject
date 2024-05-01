1f8eb8f8283dc2e9df2b18219196337049ac41ad
challa prathyusha <cprathyusha94@gmail.com>
2024-05-01 00:14:10 -0500

    adding files to the git

--- wordcount.sh
+++ wordcount.sh
@@ -32,6 +32,6 @@ echo "unique words in a file:$UNIQWORDS"
 echo $UNIQWORDS|wc -w
 for word in $UNIQWORDS
 do
-    OCCURENCES=$(echo $CONTENT|grep -o "word"|wc -l)
+    OCCURENCES=$(echo $CONTENT|grep -o "word"|wc -w)
     echo "word:$word,occurence of a word:$OCCURENCES"
 done
